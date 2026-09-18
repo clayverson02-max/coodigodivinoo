098602ec99da1ed654952dcb4e4589ebea002d18
clayverson02-max <clayverson02@gmail.com>
2026-09-18 20:09:01 -0300

    Dispara PageView no navegador e na CAPI

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { trackEvent } from "./lib/tracking";
 import "./index.css";
+
+void trackEvent("PageView");
 
 createRoot(document.getElementById("root")!).render(<App />);
